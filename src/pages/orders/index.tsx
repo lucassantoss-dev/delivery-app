9fc3ab45ecb2694bed2ebe6bdda7d75f5af8e618
lucassantossdev <lucas.santos@severinoapp.com>
2024-08-21 14:48:35 -0300

    screen of all orders

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Text } from "../../components/Text";
 import {
-    OrdersContainer,
-    OrderProvider,
-    StatusOrder,
+    Sale,
+    ProductDetails,
+    ProviderImage,
+    Separator,
     DateOrder,
     ButtonsOrder,
-    IconWrapper,
-    ImageConfiguration,
-    ResumeButton,
-    Separator,
-    CodeOrder,
+    ResumeButton
 } from "./syles";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { formatCurrency } from "../../utils/formatCurrency";
@@ -26,7 +23,6 @@ export default function Orders() {
         Promise.all([
             api.get('/sale')
         ]).then(([salesResponse]) => {
-            console.log('salesResponse', salesResponse.data.data);
             setSales(salesResponse.data.data)
         });
     }, []);
@@ -35,16 +31,34 @@ export default function Orders() {
         <>
             <FlatList
                 data={sales}
-                style={{ marginTop: 32, padding: 0 }}
+                style={{ marginTop: 32, padding: 10 }}
                 contentContainerStyle={{}}
                 keyExtractor={sale => sale._id}
                 ItemSeparatorComponent={() => <Separator />}
                 renderItem={({ item: sale }) => (
                     <>
-                        <Text size={18} weight="600" style={{textAlign: 'center'}}>
-                            Meus pedidos
-                        </Text>
-                        <OrdersContainer>
+                        <Sale>
+                            <ProviderImage
+                                source={{
+                                    uri: `${sale.photo}`
+                                }}
+                            />
+                            <ProductDetails>
+                                <Text weight="600">{sale.provider}</Text>
+                                <Text size={15} color="#15c71e" weight="600">{sale.status === true ? 'Pedido finalizado' : 'Pedido pendente'}</Text>
+                                <Text size={14} weight="600">{sale.code}</Text>
+                                <DateOrder>
+                                    <Text size={15} color="#000" weight="600">{new Date(sale.date).toLocaleDateString('pt-BR')}</Text>
+                                    <FontAwesome name="circle" size={8} color="#000" style={{ marginRight: 8, marginLeft: 12 }} />
+                                    <Text>{formatCurrency(sale.total)}</Text>
+                                </DateOrder>
+                                <ButtonsOrder>
+                                    <ResumeButton><Text size={16} color="#D73035" weight="600">Resumo</Text></ResumeButton>
+                                    <Button onPress={() => { }}>Refazer</Button>
+                                </ButtonsOrder>
+                            </ProductDetails>
+                        </Sale>
+                        {/* <OrdersContainer>
                             <IconWrapper>
                                 <ImageConfiguration source={{ uri: sale.photo }} />
                             </IconWrapper>
@@ -56,8 +70,7 @@ export default function Orders() {
                                     <Text size={15} color="#15c71e" weight="600">{sale.status === true ? 'Pedido finalizado' : 'Pedido pendente'}</Text>
                                 </StatusOrder>
                                 <CodeOrder>
-                                    <FontAwesome name="circle" size={12} color="#000" style={{ marginRight: 8, marginLeft: 12 }} />
-                                    <Text size={14} weight="600">{sale.code}</Text>
+                                    <Text size={14} weight="600" style={{marginLeft: 12}}>{sale.code}</Text>
                                 </CodeOrder>
                                 <DateOrder>
                                     <Text size={15} color="#000" weight="600">{new Date(sale.date).toLocaleDateString('pt-BR')}</Text>
@@ -69,7 +82,7 @@ export default function Orders() {
                                     <Button onPress={() => { }}>Refazer</Button>
                                 </ButtonsOrder>
                             </Separator>
-                        </OrdersContainer>
+                        </OrdersContainer> */}
                     </>
                 )}
             />
--- src/pages/orders/syles.ts
+++ src/pages/orders/syles.ts
@@ -1,68 +1,53 @@
-import { Platform, StatusBar } from "react-native";
-import styled from "styled-components/native";
+import styled from "styled-components/native"
 
-const isAndroid = Platform.OS === 'android';
-
-export const OrdersContainer = styled.SafeAreaView`
-    margin-top: 20px;
-    padding-left: 20px;
-    border: 1px solid #c3c3c3;
-    border-radius: 8px;
+export const Sale = styled.View`
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
     background-color: #fff;
-    flex-direction: row;
-    align-items: center;
+    gap: 5px;
 `;
 
-export const OrderProvider = styled.View`
-
-`;
-
-export const SettingsButton = styled.TouchableOpacity`
-    border-radius: 12px;
-`;
-
-export const IconWrapper = styled.View`
-    margin-right: 8px;
-`;
-
-export const ImageConfiguration = styled.Image`
-    width: 80px;
-    height: 80px;
-    border-radius: 40px;
-`;
-
-export const StatusOrder = styled.View`
-    padding: 1px 18px;
-    flex-direction: row;
-    align-items: center;
-`;
-export const CodeOrder = styled.View`
-    padding: 4px 6px;
-    flex-direction: row;
-    align-items: center;
-`;
-
-export const DateOrder = styled.View`
-    padding: 6px 18px;
-    flex-direction: row;
-    align-items: center;
-`;
-
-export const ButtonsOrder = styled.View`
-    flex-direction: row;
-    margin: 12px 0 0 0;
-    justify-content: center;
-    align-items: center;
-`;
-export const ResumeButton = styled.TouchableOpacity`
-    background-color: #fff;
+export const ProviderImage = styled.Image`
+    width: 90px;
+    height: 90px;
+    border-radius: 45px;
     border: 2px solid #D73035;
-    border-radius: 48px;
-    margin-right: 12px;
-     padding: 14px 24px;
+    object-fit: contain;
+    margin-left: 6px;
+`;
+
+export const ProductDetails = styled.View`
+    margin-left: 16px;
+    margin-top: 6px;
+    gap: 5px;
+    flex: 1;
 `;
 
 export const Separator = styled.View`
-    padding: 10px;
-    flex: 1;
+    width: 100%;
+    height: 1px;
+    background: rgba(204, 204, 204, 0.3);
+    margin: 1px 0;
+`;
+
+export const AddToCartButton = styled.TouchableOpacity`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+`;
+export const DateOrder = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+export const ButtonsOrder = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin: 12px 0;
+`;
+export const ResumeButton = styled.TouchableOpacity`
+    border: 2px solid #D73035;
+    padding: 11px 22px;
+    border-radius: 38px;
+    margin-right: 6px;
 `;
